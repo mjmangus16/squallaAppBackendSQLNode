506cf063cdce1fad9336b020b38f188a01ebfc52
Michael Mangus <mjmangus16@yahoo.com>
2019-08-07 09:55:27 -0400

    initial commit, server up and running

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config(); // load .env variables
+
+const server = require("./server.js");
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
